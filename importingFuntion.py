39a7e365e345b1018eb79064d427550c7f9e326b
Arkuadonis <51827906+Arkuadonis@users.noreply.github.com>
2020-07-12 06:14:39 +0530

    call funtion, square from funtions.py

---
+++ importingFuntion.py
@@ -0,0 +1,10 @@
+from funtions import square 
+
+for i in range(5):
+    print(f"the square of {i} is {square(i)}")
+
+# other method
+
+# import funtion
+# for i in range(5):
+#     print(f"the square of {i} is {funtions.square(i)}")
