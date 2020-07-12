0ba64fd38ef104d12170156fe0b48f7eae753fab
Arkuadonis <51827906+Arkuadonis@users.noreply.github.com>
2020-07-12 06:12:17 +0530

    loops in list,set

---
+++ loops.py
@@ -0,0 +1,17 @@
+#list in for loop
+for i in ["Arku", "Arun", "Pandian", "Riyas"]:
+    print(i)
+
+#set in for loop
+for i in {0,1,2,3,4,5,3,3}:
+    print(i)
+
+#python range
+for i in range(25):
+    print(i)
+
+#sequence in for loop
+name = "Arunkumar"
+for i in name:
+    print(i)
+
